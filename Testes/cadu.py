3a112b145449b6ea022429c0d60882c64234863c
Carlos Eduardo Ramos <cramos@br.ibm.com>
2018-10-24 12:50:25 -0300

    Adding cadu.py file

---
+++ Testes/cadu.py
@@ -0,0 +1,36 @@
+
+#vrvl=0
+#
+#if vrvl == 0: 
+#    print("Hello World!")
+#else:
+#    print("Welcome Back!")
+
+
+#words = ['cat', 'dog', 'rabbit']
+#
+#for animal in words:
+#    print(animal, len(animal))
+
+
+#for i in range(100):
+#    if (i%2) == 0:
+#        print(i)
+
+while True:
+    reply = input('Enter Text:')
+    if reply == 'stop': break
+    print(reply.upper())
+
+
+#print(range(10))
+#list(range(10))
+
+#from kivy.app import App
+#from kivy.uix.button import Button
+#
+#class TestApp(App):
+#    def build(self):
+#        return Button(text='Hello World')
+#
+#TestApp().run()
