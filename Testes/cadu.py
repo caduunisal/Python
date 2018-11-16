e7040b8f4e2e1d098fdb7b439576737cdc012728
Carlos Eduardo Ramos <cramos@br.ibm.com>
2018-11-16 16:10:35 -0200

    Learning Python

--- Testes/cadu.py
+++ Testes/cadu.py
@@ -466,7 +466,14 @@
 ############################################################################################################
 
 
-#Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
+############################################################################################################
+# Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus 
+# Celsius.
 #
-#    C = (5 * (F-32) / 9). 
+# C = (5 * (F-32) / 9). 
+############################################################################################################
+temp = float(input("Temperatura em Farenheit: "))
+print("Temperatura Celsius: ", (5 * (temp-32) / 9))
+
+############################################################################################################
         
